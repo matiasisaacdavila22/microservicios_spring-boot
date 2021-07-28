8e460bdf98baae416d59f8373e32d63639582209
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-07-27 23:01:16 -0300

    fix url defaultZone in registry-service

--- service-customer/service-customer/service-customer/src/main/java/academy/digitallab/store/servicecustomer/ServiceCustomerApplication.java
+++ service-customer/service-customer/service-customer/src/main/java/academy/digitallab/store/servicecustomer/ServiceCustomerApplication.java
@@ -2,7 +2,9 @@ package academy.digitallab.store.servicecustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceCustomerApplication {
 
