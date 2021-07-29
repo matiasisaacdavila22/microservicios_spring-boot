205191632ca5ac98a7e685ac3954d3e8b1ce98a9
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-07-29 17:04:31 -0300

    fix config file config-data/service-config (hystrix dashboard)

--- service-shopping/service-shopping/src/main/java/academy/digitallab/store/shopping/ServiceShoppingApplication.java
+++ service-shopping/service-shopping/src/main/java/academy/digitallab/store/shopping/ServiceShoppingApplication.java
@@ -3,16 +3,19 @@ package academy.digitallab.store.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
+
 public class ServiceShoppingApplication {
 
 	public static void main(String[] args) {
