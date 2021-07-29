40be54b94e13a94069da1a54470fc72de43b60a8
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-07-28 23:01:20 -0300

    implement fing and check of interconection entre microservicios

--- service-shopping/service-shopping/src/main/java/academy/digitallab/store/shopping/entity/InvoiceItem.java
+++ service-shopping/service-shopping/src/main/java/academy/digitallab/store/shopping/entity/InvoiceItem.java
@@ -1,5 +1,6 @@
 package academy.digitallab.store.shopping.entity;
 
+import academy.digitallab.store.shopping.model.Product;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -25,6 +26,8 @@ public class InvoiceItem  {
     @Transient // no es parte de la base, solo se utiliza para la logica
     private Double subTotal;
 
+    @Transient
+    private Product product;
 
     public Double getSubTotal(){
         if (this.price >0  && this.quantity >0 ){
